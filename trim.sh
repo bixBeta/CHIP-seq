dcb1ad33b99115e50d59b1592c74d992f684434d
faraz-epigentek <faraz+bixBeta@users.noreply.github.com>
2019-11-27 16:52:35 -0500

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -6,7 +6,7 @@ trim(){
 		mkdir TrimQC_stats fastQC trimmed_fastqs
 		for i in fastqs/*.gz
 		do
-			trim_galore --quality 20 --gzip --length 50  --fastqc --fastqc_args " --outdir ./fastQC" $i
+			trim_galore --quality 20 --gzip --length 10  --fastqc --fastqc_args " --outdir ./fastQC" $i
 		done
 		mv *_trimming_report.txt TrimQC_stats
 		mv *trimmed.fq.gz trimmed_fastqs
