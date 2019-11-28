4a33be384546522e5af2312c36ef5add6cb9f8c8
faraz-epigentek <faraz+bixBeta@users.noreply.github.com>
2019-11-28 00:44:09 -0500

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -6,7 +6,7 @@ trim(){
 		mkdir TrimQC_stats fastQC trimmed_fastqs
 		for i in fastqs/*.gz
 		do
-			trim_galore --quality 20 --gzip --length 10  --fastqc --fastqc_args " --outdir ./fastQC" $i
+			trim_galore --quality 20 --length 10 --gzip --fastqc --fastqc_args " --outdir ./fastQC" $i
 		done
 		mv *_trimming_report.txt TrimQC_stats
 		mv *trimmed.fq.gz trimmed_fastqs
@@ -14,5 +14,22 @@ trim(){
 }
 
 
-trim
+#trim
 
+
+align(){
+		for i in *.gz
+		do
+		        iSUB=`echo $i | cut -d "_" -f1`
+
+		        (bowtie2 \
+		        --local --no-unal \
+		        -x /Users/epigencare/Documents/genomes/chipGenomes/Danio_rerio/UCSC/danRer10/Sequence/Bowtie2Index/genome \
+		        -U $i \
+		        -S ${iSUB}.sam ) 2>${i}.log
+
+		done
+
+}
+
+align
