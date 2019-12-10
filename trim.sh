c79d53a06c79a282b547642a10a90c7cf9536ebf
faraz-epigentek <faraz+bixBeta@users.noreply.github.com>
2019-12-10 12:48:03 -0500

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -47,23 +47,96 @@ align(){
 # done
 
 
-for i in *.bam
-	do
-  		samtools sort $i > $(basename $i .bam).sorted.bam
-	done
+# for i in *.bam
+# 	do
+#   		samtools sort $i > $(basename $i .bam).sorted.bam
+# 	done
 
 
 
-for i in *.sorted.bam
-	do
-		samtools index $i
-	done
+# for i in *.sorted.bam
+# 	do
+# 		samtools index $i
+# 	done
 
 
-for i in *.sorted.bam
-do
-  samtools flagstat $i > $(basename  $i .sorted.bam).flagstat
-  samtools idxstats $i > $(basename  $i .sorted.bam).idxstats
-done
+# for i in *.sorted.bam
+# do
+#   samtools flagstat $i > $(basename  $i .sorted.bam).flagstat
+#   samtools idxstats $i > $(basename  $i .sorted.bam).idxstats
+# done
+
+
+# source activate homer (multiqc)
+
+
+# source activate homer 
+
+markDups(){
+
+	  for i in *.sorted.bam
+        do
+            picard \
+            MarkDuplicates \
+            INPUT=$i \
+            OUTPUT=`basename $i .sorted.bam`.dupMarked.bam \
+            ASSUME_SORTED=true \
+            REMOVE_DUPLICATES=false \
+            METRICS_FILE=`basename $i .sorted.bam`.MarkDuplicates.metrics.txt \
+            VALIDATION_STRINGENCY=LENIENT \
+            TMP_DIR=tmp
+
+        done
+}
+
+# markDups
+
+# for i in *.dupMarked.bam
+# do
+# 	samtools view -b -h -F 0X400 $i > `basename $i .dupMarked.bam`.deDup.sorted.bam
+
+# done
+
+
+# for i in *.deDup.sorted.bam; do samtools index $i ; samtools idxstats $i > `echo $i | cut -d "." -f1`.deDup.sorted.idxstats; done
+# for i in *.deDup.sorted.bam; do samtools flagstat $i > `echo $i | cut -d "." -f1`.deDup.sorted.flagstat; done
+
+
+
+
+conda activate macs2 # (macs2, python 2.6)
+
+
+macs2(){
+
+	mkdir peaks.OUT
+	
+	macs2 callpeak -t CA-1.deDup.sorted.bam \
+  	-c CA-4.deDup.sorted.bam \
+   	-f BAM -g 1400000000 \
+  	-n CA-1 \
+    --outdir peaks.OUT \
+    --keep-dup all \
+    2> CA-1-macs2.log
+
+  	macs2 callpeak -t CA-2.deDup.sorted.bam \
+  	-c CA-4.deDup.sorted.bam \
+   	-f BAM -g 1400000000 \
+  	-n CA-1 \
+    --outdir peaks.OUT \
+    --keep-dup all
+  	2> CA-2-macs2.log
+
+
+}
+
+macs2
+
+
+
+
+
+
+
 
 
